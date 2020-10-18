5c146065d3e0223a27aa9c0dd7633af881bc54a8
Kelvin Lo <kello@live.ca>
2020-10-17 18:21:48 -0700

    Add Test application for Integration Testing (#12)
    * test: add test-app for integration testing
    
    * chore: add new lines at end of files
    
    * fix: use env variable listen address instead of exposing port

---
+++ integ-test/js-sdk-emitter/server.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const tracer = require('./tracer')('example-http-server');
+// eslint-disable-next-line import/order
+const http = require('http');
+
+/** Starts a HTTP server that receives requests on sample server port. */
+function startServer(port) {
+  // Creates a server
+  const server = http.createServer(handleRequest);
+  // Starts the server
+  server.listen(port, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`Node HTTP listening on ${port}`);
+  });
+}
+
+/** A function which handles requests and send response. */
+function handleRequest(request, response) {
+  const currentSpan = tracer.getCurrentSpan();
+  // display traceid in the terminal
+  console.log(`traceid: ${currentSpan.context().traceId}`);
+  const span = tracer.startSpan('handleRequest', {
+    parent: currentSpan,
+    kind: 1, // server
+    attributes: { key: 'value' },
+  });
+  // Annotate our span to capture metadata about the operation
+  span.addEvent('invoking handleRequest');
+  try {
+    const body = [];
+    request.on('error', (err) => console.log(err));
+    request.on('data', (chunk) => body.push(chunk));
+    // const traceIdJson = returnTraceIdJson(span);
+    request.on('end', () => {
+      // deliberately sleeping to mock some action.
+      setTimeout(() => {
+        span.end();
+        response.end(JSON.stringify(returnTraceIdJson(span)));
+      }, 2000);
+    });
+  } catch (err) {
+    console.error(err);
+    span.end();
+  }
+
+  function returnTraceIdJson(span) {
+    const traceId = span.context().traceId.toString();
+    const xrayTraceId = "1-" + traceId.substring(0, 8) + "-" + traceId.substring(8);
+    const traceIdJson = { "traceId" : xrayTraceId }
+    return traceIdJson;
+  }
+  
+}
+
+startServer(process.env.LISTEN_ADDRESS);
---
+++ integ-test/js-sdk-emitter/tracer.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const { BasicTracerProvider, SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/tracing");
+const { NodeTracerProvider } = require('@opentelemetry/node');
+const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector');
+
+const { AWSXRayPropagator } = require('AWSXRayPropagator');
+const { AwsXRayIdGenerator } = require('AWSXRayIdGenerator');
+
+const { context, propagation, trace } = require("@opentelemetry/api");
+const { awsEc2Detector } = require('@opentelemetry/resource-detector-aws');
+const { detectResources } = require('@opentelemetry/resources/build/src/platform/node/detect-resources');
+
+module.exports = (serviceName) => {
+  // set global propagator
+  propagation.setGlobalPropagator(new AWSXRayPropagator());
+  
+  var resources;
+  detectResources({ detectors: [awsEc2Detector] })
+  .then((res) => {
+    resources = res;
+    console.log("detected resource: " + JSON.stringify(resources));
+  })
+    .catch((e) => {console.log(e);});
+
+  // create a provider for activating and tracking with AWS IdGenerator
+  const tracerConfig = {
+    idGenerator: new AwsXRayIdGenerator(),
+    resources: resources
+  };
+  const tracerProvider = new NodeTracerProvider(tracerConfig);
+
+  // add OTLP exporter
+  const otlpExporter = new CollectorTraceExporter({
+    serviceName: serviceName,
+    protocolNode: 2,
+  });
+  tracerProvider.addSpanProcessor(new SimpleSpanProcessor(otlpExporter));
+  tracerProvider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+  // Register the tracer
+  tracerProvider.register();
+
+  // Return an tracer instance
+  return trace.getTracer("awsxray-tests");
+}
