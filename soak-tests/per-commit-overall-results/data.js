19f7aca7cbc0313182e5947fb120519e1df64a68
github-action-benchmark <github@users.noreply.github.com>
2021-09-23 20:06:24 +0000

    add Soak Test Results - sample-app-http-manual (custombenchmark) benchmark result for 78f221a4b8c8e91b794204989694a063a2e02894

---
+++ soak-tests/per-commit-overall-results/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1632427584851,
+  "repoUrl": "https://github.com/aws-observability/aws-otel-js",
+  "entries": {
+    "Soak Test Results - sample-app-http-manual": [
+      {
+        "commit": {
+          "author": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "enowell@amazon.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "78f221a4b8c8e91b794204989694a063a2e02894",
+          "message": "Add Soak Tests (#65)\n\n* Add Soak Tests Files Unmodified from ADOT Python\r\n\r\n* Modify the Soak Tests for ADOT JS Downstream",
+          "timestamp": "2021-09-22T20:48:09Z",
+          "url": "https://github.com/aws-observability/aws-otel-js/commit/78f221a4b8c8e91b794204989694a063a2e02894"
+        },
+        "date": 1632427584419,
+        "tool": "custombenchmark",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 33.43037356321839,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 1011.6619791666667,
+            "unit": "Megabytes"
+          }
+        ]
+      }
+    ]
+  }
+}
