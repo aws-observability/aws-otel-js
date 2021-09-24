41d158946325a2f10858233bb73c16ba754140e0
github-action-benchmark <github@users.noreply.github.com>
2021-09-24 20:06:19 +0000

    add Soak Test Results - sample-app-http-manual (custombenchmark) benchmark result for 48eff4e97841c6ea694cc8190836b240d7b1e852

--- soak-tests/per-commit-overall-results/data.js
+++ soak-tests/per-commit-overall-results/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632427584851,
+  "lastUpdate": 1632513979953,
   "repoUrl": "https://github.com/aws-observability/aws-otel-js",
   "entries": {
     "Soak Test Results - sample-app-http-manual": [
@@ -31,6 +31,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak Test Average Virtual Memory Used",
             "value": 1011.6619791666667,
+            "unit": "Megabytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "enowell@amazon.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "48eff4e97841c6ea694cc8190836b240d7b1e852",
+          "message": "Edits for compatability with aws-otel-collector v13 (#66)",
+          "timestamp": "2021-09-23T20:10:47Z",
+          "url": "https://github.com/aws-observability/aws-otel-js/commit/48eff4e97841c6ea694cc8190836b240d7b1e852"
+        },
+        "date": 1632513979257,
+        "tool": "custombenchmark",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 31.075775862068966,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 1035.4220052083333,
             "unit": "Megabytes"
           }
         ]
